36df3fc76dda784c54927a74f768b57188ccdb7e
zasi0001 <112558241+zmeincke@users.noreply.github.com>
2023-01-07 23:13:39 +0100

    profil mobil css

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -4,35 +4,37 @@ window.addEventListener("load", sidenVises);
 const button = document.querySelector(".toggle_button");
 // Lav variablen "menu", der henviser til ".main-menu"
 const menu = document.querySelector("#main_menu");
-const ulmenu = document.querySelector(".ul_menu");
 
 function sidenVises() {
     console.log("siden vises");
-    button.classList.add(".icon_menu");
+    //tilføj icon til button
+    button.classList.add("icon_menu");
 }
 
 
-
-// Lav funktionen "toggleMenu()" med følgende funktionalitet
+// Lav funktionen "toggleMenu()"
 function toggleMenu() {
-    console.log("button clicked")
+    console.log("button clicked");
 
-    // a) toggle klassen "shown" på menu
+    // tilføj toggle klassen "shown" på menu
     menu.classList.toggle("show");
 
 
-    // spørg om "menuShow" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+    // spørg om "menu" i if-sætningen nedenfor 
     if (menu.classList.contains("show")) {
-        console.log(menu.classList.contains("show")); // se i konsollen
-        // sæt btn.textContent til "Luk", hvis menuShown er "true"
-        button.classList.remove("toggle_button")
+        console.log(menu.classList.contains("show"));
+
+        // sæt bagrundsbilled til "lukke icon", når toggleMenu er "true"
+        button.classList.remove("icon_menu");
         button.classList.add("icon_exit");
     } else {
-        console.log(menu.classList.contains("show")); // se i konsollen
-        // sæt btn.textContent til "Menu", hvis menuShown er "false"
+        console.log(menu.classList.contains("show"));
+
+        // sæt bgrundsbilled til "menu icon", når toggleMenu er "false"
+        button.classList.remove("icon_exit");
         button.classList.add("icon_menu");
     }
 }
 
-// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
+// Tilføj et klik-event til "button", der kalder på "toggelMenu" funktionen
 button.addEventListener("click", toggleMenu);
